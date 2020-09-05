eae7cdce6b11933fc16a729e19fc9650ef8a333e
krivax <krivahz@gmail.com>
2020-09-05 18:05:22 -0300

    adicionado o filtro por data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,10 +3,12 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
+import { uuid } from 'uuidv4';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
 import formatCurrency from '../../utils/formatCurrency';
 import formatDate from '../../utils/formatDate';
+import monthsList from '../../utils/months';
 
 import { Container, Content, Filters} from './styles';
 
@@ -66,7 +68,7 @@ const List: React.FC<IRouteParams> = ({ match })=> {
     const dataFormated = filterDate.map(item => {
       
       return {
-        id: String(new Date().getTime()) + item.amount,
+        id: uuid(),
         description: item.description,
         amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
@@ -76,28 +78,36 @@ const List: React.FC<IRouteParams> = ({ match })=> {
     })
     setData(dataFormated);
   },[listData, monthSelected, yearSelected, data.length]);
+  
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
 
-  const months = [
-    {value: 1, label: 'Janeiro'},
-    {value: 2, label: 'Fevereiro'},
-    {value: 3, label: 'Março'},
-    {value: 4, label: 'Abril'},
-    {value: 5, label: 'Maio'},
-    {value: 6, label: 'Junho'},
-    {value: 7, label: 'Julho'},
-    {value: 8, label: 'Agosto'},
-    {value: 9, label: 'Setembro'},
-    {value: 10, label: 'Outubro'},
-    {value: 11, label: 'Novembro'},
-    {value: 12, label: 'Dezembro'}
-  ]
-  
-  const years = [
-    {value: 2020, label: 2020},
-    {value: 2019, label: 2019},
-    {value: 2018, label: 2018}
-  ]
-  
+    listData.forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if(!uniqueYears.includes(year)){
+        uniqueYears.push(year)
+      }
+    });
+
+    return uniqueYears.map(year => {
+      return{
+        value: year,
+        label: year,
+      }
+    });
+  },[listData]);
+
+  const months = useMemo(() => {
+    
+    return monthsList.map((month, index) => {
+      return{
+        value: index + 1,
+        label: month,
+      }
+    });
+  },[]);
 
   return(
     <Container>
